3a6f812996aec5635f2f7fcad9e2f5e3af2aa4da
keon <power3869@gmail.com>
2022-08-01 17:19:20 +0900

    use server data

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import ReviewList from "./ReviewList";
-import mockItems from '../mock.json';
 import { useState } from "react";
+import { getReviews } from "../api";
 
 function App() {
-	const [items, setItems] = useState(mockItems);
+	const [items, setItems] = useState([]);
 	const [order, setOrder] = useState('createdAt');
 	const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -14,7 +14,12 @@ function App() {
 	const handleDelete = (id) => {
 		const nextItems = items.filter((item) => item.id !== id);
 		setItems(nextItems);
-	} 
+	};
+
+	const handleLoadClick = async () => {
+		const { reviews } = await getReviews();
+		setItems(reviews);
+	};
 
 	return (
 		<div>
@@ -23,6 +28,7 @@ function App() {
 				<button onClick={handleBestClick}>베스트순</button>
 			</div>
 			<ReviewList items={sortedItems} onDelete={handleDelete}/>
+			<button onClick={handleLoadClick}>불러오기</button>
 		</div>
 	);
 }
